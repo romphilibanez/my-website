f884102e5794a666e603cbf70662b3affe371431
Joseph Ibanez <69676508+romphilibanez@users.noreply.github.com>
2025-02-09 04:09:18 +0800

    Add files via upload
    Commit changes

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".project-card img");
+
+    images.forEach(img => {
+        img.addEventListener("click", () => {
+            const modal = document.createElement("div");
+            modal.classList.add("modal");
+            modal.innerHTML = `
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <img src="${img.src}" alt="${img.alt}">
+                </div>
+            `;
+            document.body.appendChild(modal);
+
+            modal.querySelector(".close").addEventListener("click", () => {
+                modal.remove();
+            });
+        });
+    });
+});
